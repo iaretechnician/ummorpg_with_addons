7f4304a2963454cfbf792a29e4aa28f6d2fb9671
ConveyorX (Mohan pal singh ubhi) <88631194+ConveyorX@users.noreply.github.com>
2024-11-12 21:21:31 +0300

    Add files via upload

---
+++ Scripts/LanguageChanger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using I2.Loc;
+
+public class LanguageChanger : MonoBehaviour
+{
+	public string currentLanguage = "English";
+	
+	private void Start()
+	{
+		LocalizationManager.CurrentLanguage = currentLanguage;		
+	}
+}
