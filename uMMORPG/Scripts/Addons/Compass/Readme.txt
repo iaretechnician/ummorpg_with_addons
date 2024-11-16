e26e004c7aebd0cf27d0b7426be81d5fa96979e8
ConveyorX (Mohan pal singh ubhi) <88631194+ConveyorX@users.noreply.github.com>
2024-11-16 22:30:25 +0300

    Add files via upload

---
+++ uMMORPG/Scripts/Addons/Compass/Readme.txt
@@ -0,0 +1,5 @@
+Installation of UI_Compass
+
+add the UI_Compass prefab to the canvas, place it where you want
+
+finished... 
---
+++ uMMORPG/Scripts/Addons/Compass/Script/Compass.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Compass : MonoBehaviour
+{
+    public GameObject panel;
+    public RawImage compass;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Player player = Player.localPlayer;
+        if (player)
+        {
+            panel.SetActive(true);
+            float uvx = player.movement.transform.localEulerAngles.y;
+            compass.uvRect = new Rect(uvx / 360f, 0, 1, 1);
+        }
+        else panel.SetActive(false);
+    }
+}
